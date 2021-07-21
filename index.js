9cde6878b959a06dc802640b7eb02baf1377671a
Kade Griffith <kadeg95@gmail.com>
2021-07-21 12:26:51 -0600

    finished all tasks and stretch tasks. Started a codePen where I am digging deep into the car contructor function, playing around with new methods, conditionals in a method, etc.

--- index.js
+++ index.js
@@ -43,20 +43,11 @@ function Person(name, age) {
   this.name = name;
   this.age = age;
   this.stomach = [];
-  this.eat = function(someFood){
-    if(this.stomach.length < 10){
-      return this.stomach.push(someFood);
-    }
-  };
-  this.poop = function(){
-    return this.stomach = [];
-  };
-  this.toString = function(){
-    return this.name + ', ' + this.age;
-  }
 }
 
-
+Person.prototype.eat = function(someFood){if(this.stomach.length < 10){ return this.stomach.push(someFood)}}
+Person.prototype.poop =function(){return this.stomach = []};
+Person.prototype.toString = function(){return this.name + ', ' + this.age}
 
 
 
@@ -76,11 +67,23 @@ function Person(name, age) {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+  this.fuel = 0;
 }
 
-
+Car.prototype.fill = function(gallons){return this.tank += gallons};
+Car.prototype.drive = function(distance){
+  if(this.fuel === 0){
+    return `I ran out of fuel at ${this.odometer}`
+  }else{
+    this.odometer += distance;
+    this.tank -= distance/mpg;
+  }
+};
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -88,18 +91,24 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window: 'this' is binded to window by default, global object in node, or returns undefined in strict mode
+  2. Implicit: 'this' is binded to whatever is left of the dot
+  3. Explicit: 'this' is binded to what you specify after .call .bind or .apply
+  4. New: 'this' is binded to the object you will be creating when you call the constructor function.
 */
 
 
